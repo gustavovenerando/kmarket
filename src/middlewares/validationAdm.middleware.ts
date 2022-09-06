3358ceaca5c814dd3187a68fb5f32553a60ecbb9
Gustavo H <gvhenrique@hotmail.com>
2022-09-06 11:45:12 -0300

    feat: validation middleware

---
+++ src/middlewares/validationAdm.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import AppError from "../errors/AppError";
+
+const validationAdmMiddleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const isAdm = req.employee.isAdm;
+
+	if (!isAdm) {
+		throw new AppError(403, "Adm permission requested.");
+	}
+
+	next();
+};
+
+export default validationAdmMiddleware;
