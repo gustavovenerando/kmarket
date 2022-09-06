1380f896c3056dbe4f34f63916d1c1c70c4c697c
Matheus Santos <eumtssantos@outlook.com>
2022-09-06 13:51:13 -0300

    feat: created supplierProdutcs entity

---
+++ src/entities/supplierProducts.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("supplierProducts")
+export class SupplierProduct {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: "decimal", precision: 10, scale: 2 })
+  costPrice: number;
+
+  //@ManyToOne(()=> Supplier) arrumo assim que subir tudo;
+
+  //   @ManyToOne(()=> Category) arrumo assim que subir tudo;
+}
