a439e3f13d8862948632961f379c1442875651ec
Daniel Josias <daniieljosiaslima@gmail.com>
2022-09-07 14:12:53 -0300

    fix: crud employee

--- src/interfaces/employee/index.ts
+++ src/interfaces/employee/index.ts
@@ -2,3 +2,11 @@ export interface IEmployeeLogin {
 	email: string;
 	password: string;
 }
+
+export interface IEmployeeRequest {
+	name: string;
+	email: string;
+	password: string;
+	isAdm: boolean;
+	isActive: boolean;
+}
