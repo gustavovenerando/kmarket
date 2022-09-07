2b550894da1c02033c8c094390ff8f2ea1e35f09
Gustavo H <gvhenrique@hotmail.com>
2022-09-07 15:11:16 -0300

    fix: empoloyees router

--- src/controllers/employees.controllers.ts
+++ src/controllers/employees.controllers.ts
@@ -1,12 +1,22 @@
-import { Request, Response } from 'express'
-import { createEmployeesService } from '../services/employees/createEmployees.services'
-import { IEmployeeRequest } from '../interfaces/employee'
-import { instanceToPlain } from 'class-transformer'
+import { Request, Response } from "express";
+import { createEmployeesService } from "../services/employees/createEmployees.services";
+import { IEmployeeRequest } from "../interfaces/employee";
+import { instanceToPlain } from "class-transformer";
 
-export const createEmployeesController = async (req: Request, res: Response) => {
-    
-    const { name, email, password, isAdm, isActive }:IEmployeeRequest = req.body
+export const createEmployeesController = async (
+	req: Request,
+	res: Response
+) => {
+	const { name, email, password, isAdm, isActive }: IEmployeeRequest =
+		req.body;
 
-    const employee = await createEmployeesService({name,email,password,isAdm,isActive})
-    return res.status(201).json(instanceToPlain(employee))
-}
+	const employee = await createEmployeesService({
+		name,
+		email,
+		password,
+		isAdm,
+		isActive,
+	});
+
+	return res.status(201).json(instanceToPlain(employee));
+};
