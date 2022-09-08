e87c6623fe643414144ed14caa5851c7460ed693
Gustavo H <gvhenrique@hotmail.com>
2022-09-08 15:12:22 -0300

    fix: mocks

---
+++ src/services/employees/createEmployees.services.ts
@@ -0,0 +1,57 @@
+import AppDataSource from "../../data-source";
+import Employee from "../../entities/employee.entity";
+import { IEmployeeRequest } from "../../interfaces/employee/index";
+import { hash } from "bcryptjs";
+
+import AppError from "../../errors/AppError";
+
+export const createEmployeesService = async ({
+	name,
+	email,
+	password,
+	isAdm,
+	isActive,
+}: IEmployeeRequest): Promise<Employee> => {
+	const employeesRepository = AppDataSource.getRepository(Employee);
+	const employees = await employeesRepository.find();
+
+	const emailAlreadyExists = employees.find(
+		(employee) => employee.email === email
+	);
+
+	if (emailAlreadyExists) {
+		throw new AppError(400, "Email already exists");
+	}
+
+	if (
+		name !== undefined ||
+		email !== undefined ||
+		password !== undefined ||
+		isAdm !== undefined ||
+		isActive !== undefined
+	) {
+		throw new AppError(400, "Required field");
+	}
+
+	const hashPassword = await hash(password, 10);
+
+	const employee = employeesRepository.create({
+		name,
+		email,
+		password: hashPassword,
+		isAdm,
+		isActive,
+	});
+
+	// const employee = new Employee()
+	// employee.name = name
+	// employee.email = email
+	// employee.password = hashPassword
+	// employee.isAdm = isAdm
+	// employee.isActive = isActive
+
+	// employeesRepository.create(employee)
+	await employeesRepository.save(employee);
+
+	return employee;
+};
