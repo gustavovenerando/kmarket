a07398419a53d28683c65b1c11b5b6f24dbbc3ad
Gustavo H <gvhenrique@hotmail.com>
2022-09-08 17:21:25 -0300

    feat: first categories tests; fix: package.json

---
+++ src/__tests__/integration/categories/categoriesRoutes.spec.ts
@@ -0,0 +1,67 @@
+import { DataSource } from "typeorm";
+import AppDataSource from "../../../data-source";
+import request from "supertest";
+import app from "../../../app";
+import {
+	mockedAdm,
+	mockedCategory,
+	mockedEmployee,
+	mockedLoginAdm,
+} from "../../mocks";
+
+describe("/categories", () => {
+	let connection: DataSource;
+
+	beforeAll(async () => {
+		await AppDataSource.initialize()
+			.then((res) => {
+				connection = res;
+			})
+			.catch((err) => {
+				console.error("Error during Data Source initialization", err);
+			});
+
+		await request(app).post("/employees").send(mockedEmployee);
+		await request(app).post("/employees").send(mockedAdm);
+	});
+
+	afterAll(async () => {
+		await connection.destroy();
+	});
+
+	test("POST /categories -  Must be able to create category", async () => {
+		const adminLoginResponse = await request(app)
+			.post("/login")
+			.send(mockedLoginAdm);
+		const response = await request(app)
+			.post("/categories")
+			.set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+			.send(mockedCategory);
+
+		expect(response.body).toHaveProperty("name");
+		expect(response.body).toHaveProperty("id");
+		expect(response.status).toBe(201);
+	});
+
+	test("POST /categories -  should not be able to create category that already exists", async () => {
+		const adminLoginResponse = await request(app)
+			.post("/login")
+			.send(mockedLoginAdm);
+		const response = await request(app)
+			.post("/categories")
+			.set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+			.send(mockedCategory);
+
+		expect(response.body).toHaveProperty("message");
+		expect(response.status).toBe(409);
+	});
+
+	test("POST /categories -  should not be able to create category without authentication", async () => {
+		const response = await request(app)
+			.post("/categories")
+			.send(mockedCategory);
+
+		expect(response.body).toHaveProperty("message");
+		expect(response.status).toBe(401);
+	});
+});
--- src/services/categories/createCategory.service.ts
+++ src/services/categories/createCategory.service.ts
@@ -1,9 +1,17 @@
 import AppDataSource from "../../data-source";
 import Category from "../../entities/categories.entity";
+import AppError from "../../errors/AppError";
 
 const createCategoryService = async (name: string) => {
-  const categoryRepository = AppDataSource.getRepository(Category);
-  const categoryCreated = await categoryRepository.save({ name });
-  return categoryCreated;
+	const categoryRepository = AppDataSource.getRepository(Category);
+
+	const categoryAlreadyExists = await categoryRepository.findOneBy({ name });
+
+	if (categoryAlreadyExists) {
+		throw new AppError(409, "Category already exists.");
+	}
+
+	const categoryCreated = await categoryRepository.save({ name });
+	return categoryCreated;
 };
 export default createCategoryService;
