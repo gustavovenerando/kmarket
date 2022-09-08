5cbe39b6894187c40e1441408ca24ccdb2bc414f
Gustavo Venerando <gvhenrique@hotmail.com>
2022-09-08 10:32:28 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,7 @@ import sessionRouter from "./routers/session.routes";
 import cartRoutes from "./routers/carts.routes";
 import supplierRoutes from "./routers/suppliers.routes";
 import loyaltyCustomerRoutes from "./routers/loyaltyCustomer.routes";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import employeeRouter from './routers/employees.routes'
-=======
-import employeeRouter from "./routers/employees.routes";
->>>>>>> b0896b27732745cb2848ca9a5d3e3bbaaa81d62d
-=======
-import employeeRouter from "./routers/employees.routes";
->>>>>>> 8d6c803e168c0a8831688b24d4bcc16839d678d2
 import categoriesRoutes from "./routers/categories.routes";
 import productsRouter from "./routers/products.routes";
 import authTokenMiddleware from "./middlewares/authToken.middleware";
