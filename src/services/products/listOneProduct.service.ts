d8e524f534a8678f325ad5aa5228918b4864adc2
Gustavo H <gvhenrique@hotmail.com>
2022-09-09 14:01:24 -0300

    fix: get products by id route

--- src/services/products/listOneProduct.service.ts
+++ src/services/products/listOneProduct.service.ts
@@ -5,7 +5,7 @@ import AppError from "../../errors/AppError";
 const listOneProductsService = async (id: string) => {
 	const productsRepository = AppDataSource.getRepository(Products);
 
-	const product = productsRepository.findOneBy({ id });
+	const product = await productsRepository.findOneBy({ id });
 
 	if (!product) {
 		throw new AppError(404, "Product not found");
